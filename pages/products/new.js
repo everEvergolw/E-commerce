19584e50e35c2dba8e5d97031e178024d75656f7
everEvergolw <1092547508@qq.com>
2024-07-16 16:28:32 +0100

    design:product management

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,11 +1,76 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function NewProduct(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [goToProducts, setGoToProducts] = useState(false);
+    const router = useRouter();
+
+    async function createProduct(e){
+        e.preventDefault();  
+
+        const data = { title, description, price };
+        
+            await axios.post('/api/products', data);
+            setGoToProducts(true);
+        
+
+    }
+
+
+    if(goToProducts){ 
+         router.push('/products');
+    } 
+
     return(
+
+
         <Layout>
 
+            <form onSubmit={createProduct} >
 
-                    <input type="text" placeholder="product name" />
+                <h1>  New Product  </h1>
+                        <label> Product name </label>
+                        <input 
+                            type="text" 
+                            placeholder="product name" 
+                            value={title}
+                            onChange={ev => setTitle(ev.target.value)}
+                            
+                        />
+
+                        <label> Description  </label>
+
+                        <textarea 
+                            placeholder="description"
+                            value={description}
+                            onChange={ev => setDescription(ev.target.value)} 
+
+                            >
+
+
+                        </textarea>
+
+                        <label> Price (in USD)  </label>
+
+                        <input 
+                            type="number" 
+                            placeholder="price" 
+                            value={price}
+                            onChange={ev => setPrice(ev.target.value)}
+
+                            />
+
+                        <button 
+                            type="submit"
+                            className="btn-primary"> Save</button>
+
+
+            </form>
 
 
         </Layout>
